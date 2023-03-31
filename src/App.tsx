6b1f4b6c2b541757b8a18bdb3006162821322d58
Kamil Nowak <kamil0nowak@yahoo.com>
2023-03-31 09:18:16 +0200

    initial setup, tailwind, favicons, etc

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "./components/Footer"
+
+const App = () => {
+
+  return (
+    <>
+      <h2>Lorem, ipsum dolor.</h2>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+type Props = {}
+
+export const Footer = (props: Props) => {
+    return (
+        <footer>
+            <h4 className="text-center">
+                Crafted with ❤ by <a className="" href="#">Kamil</a> for students.
+            </h4>
+            <h5 className="text-center text-sm">
+                Thank you <strong>Freepik</strong> for favicon!
+            </h5>
+        </footer>
+    )
+}
